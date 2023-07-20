9e01ee4375110c6bd5552902a7c9b7224114090f
ArijitNaskar12 <arijitnaskar378@gmail.com>
2023-07-20 16:38:47 +0530

    Weather App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App=()=>{
+    const[longitude,setLongitude]=useState(0);
+    const[latitude,setLatitude]=useState(0);
+    const[hemisphere,setHemisphere]=useState("");
+    const[month,setMonth]=useState(new Date().getMonth());
+    
+function getLocation(){
+        if(navigator.geolocation)
+        {
+            navigator.geolocation.getCurrentPosition((location)=>{
+                setLongitude(location.coords.longitude);
+                setLatitude (location.coords.latitude);
+
+                if(location.coords.latitude>0)
+                {
+                    setHemisphere("Northern hemisphere");
+                }else if(location.coords.latitude<0)
+                {
+                    setHemisphere("Southern hemisphere")
+                }else{
+                    setHemisphere("Equator");
+                }
+        
+            });
+        }
+    
+      
+    }
+    return  (
+        <div>
+            <h1>Longitude: {longitude}</h1>
+            <h1>Latitude:{latitude}</h1>
+            <h1>Hemisphere:{hemisphere}</h1>
+            <button onClick={getLocation}>Get Location</button>
+            {
+               (hemisphere==="Northern hemisphere" && (month>=2 && month<=9)) ||
+                (hemisphere=="Souhtern hemisphere" &&(month<2 ||month>9)) ?
+                (
+                    <div>
+                        <h1>It's Summer in the {hemisphere}</h1>
+                        <p>आया मौसम थंडे थंडे पावसाळे का</p>
+                        <img src="https://i.ytimg.com/vi/HzErgJjLN0A/maxresdefault.jpg"
+                        alt="summer"
+                        />
+                    </div>
+                ):(
+                    ( hemisphere==="Northern hemisphere" && (month<2 || month>9))
+                     ||  (hemisphere==="Southern hemisphere" && month>=2 && month<=9)?
+                     (
+                        <div>
+                        <h1>It's Winter in the {hemisphere}</h1>
+                        <p>आया मौसम ठंडा ठंडा पावसाळे का</p>
+                        <img src="https://pbs.twimg.com/media/DpePTTuWsAAxmhT.jpg" 
+                        alt="winter"
+                        />
+                    </div>
+                     ):
+                        ( <h1>Relax we are fetching ... </h1>)
+                )
+            }
+            </div>
+    )
 }
-
 export default App;
